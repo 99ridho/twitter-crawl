af950c0785e37667b9bddc6fac83638648064179
Ridho Pratama <99ridho@gmail.com>
2020-10-16 03:03:44 +0700

    use v1.1 instead

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from asyncio import (get_event_loop)
 async def main():
     svc = TwitterService()
     tweets = await svc.get_recent_tweet_search('omnibus law')
-
-    print(tweets)
+    for tweet in tweets:
+        print(tweet)
 
 loop = get_event_loop()
 loop.run_until_complete(main())
--- twitter_service.py
+++ twitter_service.py
@@ -3,22 +3,33 @@ from typing import (Optional, Any, Dict, List)
 from config import BEARER_TOKEN
 
 class TweetSearchData:
-    def __init__(self, id: str, text: str):
+    def __init__(self, id: str, text: str, type: str):
         self.id = id
         self.text = text
+        self.type = type
 
     def __repr__(self):
-        return f'TweetSearchData(id = {self.id}, text = {self.text})'
+        return f'TweetSearchData(id = {self.id}, text = {self.text}, type = {self.type})'
 
 class TwitterService:
     def __init__(self):
         self.http_client = HTTPClient()
         self.bearer_token = BEARER_TOKEN
 
+    def __make_search_data(self, tweet: Any) -> TweetSearchData:
+        return TweetSearchData(
+            id=tweet['id_str'],
+            text=tweet['full_text'],
+            type=tweet['metadata']['result_type']
+        )
+
     async def get_recent_tweet_search(self, query: str) -> Optional[List[TweetSearchData]]:
         params: Dict[str, any] = {
-            'query': query,
-            'max_results': 100
+            'q': f'{query} -filter:retweets',
+            'count': 100,
+            'include_entities': 'false',
+            'tweet_mode': 'extended',
+            'result_type': 'mixed'
         }
 
         headers: Dict[str, str] = {
@@ -26,8 +37,8 @@ class TwitterService:
         }
 
         request_data = HTTPRequestData(
-            'https://api.twitter.com/2',
-            path='/tweets/search/recent',
+            'https://api.twitter.com/1.1',
+            path='/search/tweets.json',
             method='GET',
             params=params,
             headers=headers
@@ -38,6 +49,6 @@ class TwitterService:
         if json is None:
             return None
 
-        tweets = set(map(lambda raw_tweet: TweetSearchData(id=raw_tweet['id'], text=raw_tweet['text']), json['data']))
+        tweets = set(map(self.__make_search_data, json['statuses']))
 
         return tweets
